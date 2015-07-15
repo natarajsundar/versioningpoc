1afd4e62e8ada2d5864459742fbfc4c196ad4e06
unknown <Z001L5H@HB97Q12.dist.target.com>
2015-07-14 22:21:27 -0700

    Checking in versioning with Spring POC

---
+++ src/main/java/com/pd/test/spring/CustomRequestMappingHandlerMapping.java
@@ -0,0 +1,33 @@
+package com.pd.test.spring;
+
+import com.pd.test.spring.version.VersionedResourceRequestCondition;
+import com.pd.test.spring.version.VersionedResource;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.web.servlet.mvc.condition.RequestCondition;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.lang.reflect.Method;
+
+public class CustomRequestMappingHandlerMapping extends RequestMappingHandlerMapping {
+
+    @Override
+    protected RequestCondition<?> getCustomTypeCondition(Class<?> handlerType) {
+        VersionedResource typeAnnotation = AnnotationUtils.findAnnotation(handlerType, VersionedResource.class);
+        return createCondition(typeAnnotation);
+    }
+
+    @Override
+    protected RequestCondition<?> getCustomMethodCondition(Method method) {
+        VersionedResource methodAnnotation = AnnotationUtils.findAnnotation(method, VersionedResource.class);
+        return createCondition(methodAnnotation);
+    }
+
+    private RequestCondition<?> createCondition(VersionedResource versionMapping) {
+        if (versionMapping != null) {
+            return new VersionedResourceRequestCondition(versionMapping.media(), versionMapping.from(), versionMapping.to());
+        }
+
+        return null;
+    }
+
+}
